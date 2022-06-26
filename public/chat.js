7093560bf3057c7df204427635e99cb29fba2a3f
kowais915 <kowais915@gmail.com>
2022-06-27 03:11:32 +0500

    setup the ui for chat

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,24 @@
 console.log("Working")
 var socket = io.connect("http://localhost:3000/");
+
+
+let username = document.getElementById('handle'),
+    msg = document.getElementById('message'),
+    output = document.getElementById('output'),
+    send = document.getElementById('send');
+
+
+   
+
+    send.addEventListener("click", (e=>{
+
+        alert(msg.value)
+        alert(username.value)
+
+        socket.emit('chat', {
+            msg: msg.value,
+            username: username.value
+        })
+
+     
+    }) )
